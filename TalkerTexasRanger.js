a8f34bee0cb7e422eae6a65db2bbb59b9592f0b3
Patric Nordmark <patric.nordmark@gmail.com>
2012-01-10 23:57:13 +0100

    Removed inclusion of tls in TalkerTexasRanger.js and prettified it

--- TalkerTexasRanger.js
+++ TalkerTexasRanger.js
@@ -1,6 +1,5 @@
-var tls = require('tls'),
-    fs = require('fs'),
-    talker = require('./lib/talker.js');
+var fs = require('fs'),
+    talker = require('./lib/talker');
 
 // Output Walker Texas Ranger ASCII.
 console.log(fs.readFileSync('./lib/chucknorris_ascii.txt', 'utf8').grey);
