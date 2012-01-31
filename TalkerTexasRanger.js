ec6fedfde482446b0db8c80423738c02198ee84e
Patric Nordmark <patric.nordmark@gmail.com>
2012-01-31 11:36:28 +0100

    plugins to plugin, rooms to room. and added the collection of plugins/rooms as child object instead.

--- TalkerTexasRanger.js
+++ TalkerTexasRanger.js
@@ -7,4 +7,4 @@ console.log(fs.readFileSync('./lib/chucknorris_ascii.txt', 'utf8').grey);
 // Connect to TalkerApp.
 talker.connect();
 // Load all plugins specified in config file.
-talker.plugins.loadAll();
+talker.plugin.loadAll();
